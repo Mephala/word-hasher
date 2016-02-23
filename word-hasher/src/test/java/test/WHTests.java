c3e6358d8816bcc3e02544aab5e222f3d24a8c63
Mephala <gokhan.ozgozen@gmail.com>
2016-02-23 12:07:00 +0200

    case added.

--- word-hasher/src/test/java/test/WHTests.java
+++ word-hasher/src/test/java/test/WHTests.java
@@ -1,8 +1,6 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import mockit.integration.junit4.JMockit;
 
@@ -11,19 +9,52 @@ import org.junit.runner.RunWith;
 
 import com.gokhanozg.wordhasher.WordHasher;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(JMockit.class)
 public class WHTests {
 
-	@Test
-	public void testConstruction() {
+    @Test
+    public void testConstruction() {
 
-		int wordCount = 1000;
-		List<String> wordz = new ArrayList<String>();
-		for (int i = 0; i < wordCount; i++) {
-			wordz.add(UUID.randomUUID().toString().substring(0, 5));
-		}
-		long start = System.currentTimeMillis();
-		WordHasher wh = new WordHasher(wordz, 10);
-		System.err.println("Construction took " + (System.currentTimeMillis() - start) + " ms.");
-	}
+        int wordCount = 10000;
+        List<String> wordz = new ArrayList<String>();
+        for (int i = 0; i < wordCount; i++) {
+            wordz.add(UUID.randomUUID().toString().substring(0, 5));
+        }
+        long start = System.currentTimeMillis();
+        WordHasher wh = new WordHasher(wordz);
+        System.err.println("Construction took " + (System.currentTimeMillis() - start) + " ms.");
+    }
+
+    @Test
+    public void testSearching1() throws InterruptedException {
+        List<String> searchKeywords = new ArrayList<String>();
+        McaProjectEAR[] ears = McaProjectEAR.values();
+        for (McaProjectEAR ear : ears) {
+            searchKeywords.add(ear.toString());
+        }
+        WordHasher wordHasher = new WordHasher(searchKeywords);
+        assertTrue(wordHasher.search("gen") != null && wordHasher.search("gen").size() > 0);
+    }
+
+    @Test
+    public void testSearching2() throws InterruptedException {
+        List<String> searchKeywords = new ArrayList<String>();
+        searchKeywords.add("gokhan");
+        searchKeywords.add("gokhan ozgozen");
+        searchKeywords.add("gokhan ozgozen hudavendigar");
+        searchKeywords.add("gokhan ozgozen hudavendigar merkezi");
+        searchKeywords.add("gokhan ozgozen hudavendigar merkezi yalitim");
+        searchKeywords.add("gokhan ozgozen hudavendigar merkezi yalitim bu kapagin");
+        searchKeywords.add("gokhan ozgozen hudavendigar merkezi yalitim bu kapagin altinda");
+        searchKeywords.add("gokhan ozgozen hudavendigar merkezi yalitim bu kapagin altinda iyi bankadir");
+        searchKeywords.add("gokhan ozgozen hudavendigar merkezi yalitim bu kapagin altinda iyi bankadir yoksa");
+        searchKeywords.add("gokhan ozgozen hudavendigar merkezi yalitim bu kapagin altinda iyi bankadir yoksa baska");
+        searchKeywords.add("gokhan ozgozen hudavendigar merkezi yalitim bu kapagin altinda iyi bankadir yoksa baska bir");
+        searchKeywords.add("gokhan ozgozen hudavendigar merkezi yalitim bu kapagin altinda iyi bankadir yoksa baska bir arzunuz");
+        WordHasher wordHasher = new WordHasher(searchKeywords,40,new Locale("tr-TR"));
+        Set<String> retval = wordHasher.search("DİG");
+        System.out.println(retval);
+    }
 }
